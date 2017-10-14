15ee612c0c751166e635d50f74429a773fa57dc8
khaiharnn <khaiharnn@gmail.com>
2017-10-14 10:39:15 +0700

    Variable Type

--- BasicSwift4_14oct2017.playground/Contents.swift
+++ BasicSwift4_14oct2017.playground/Contents.swift
@@ -1,5 +1,16 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, Master"
+
+// Variable and Constant การประกาศตัวแปรและค่าคงที่
+// การประกาศตัวแปรแบบแก้ไขค่าได้
+
+var nameString = "Doramon"
+var numberInt = 123
+var numberDouble = 12.345
+var statusBool = true
+
+nameString = "Doramee"
+numberInt = 222
+
+
+
