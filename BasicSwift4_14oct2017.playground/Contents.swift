b7bba03f5735c15a0cb57176ee68948cd00489ad
khaiharnn <khaiharnn@gmail.com>
2017-10-14 10:55:41 +0700

    Finish

--- BasicSwift4_14oct2017.playground/Contents.swift
+++ BasicSwift4_14oct2017.playground/Contents.swift
@@ -12,5 +12,15 @@ var statusBool = true
 nameString = "Doramee"
 numberInt = 222
 
+//การประกาศตัวแปรที่ไม่สามารถแก้ไขค่าได้
+let surnameString = "MySurname"
+//surnameString = "ChangeSur"
 
+//การกำหนด Data Type
+var studentString:String = "Nobita"
+var number2Int:Int = 333
 
+//Display on Terminal
+print("Display on Terminal")
+print("name = \(nameString)")
+print ("number = \(numberInt + 100)")
