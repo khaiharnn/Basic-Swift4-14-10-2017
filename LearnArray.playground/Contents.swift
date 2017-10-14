9e4e1c735b317a1bb329a1d8a2e74271cf2c822c
khaiharnn <khaiharnn@gmail.com>
2017-10-14 14:14:34 +0700

    Success

--- LearnArray.playground/Contents.swift
+++ LearnArray.playground/Contents.swift
@@ -2,4 +2,24 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var nameArrayString = ["AAA","BBB","CCC","DDD"]
+print ("nameArray ต่ำแหน่ง 2 = \(nameArrayString[2])")
+
+
+// ให้แสดงผลของ nameArrayString[0] +  nameArrayString[3]
+print ("\(nameArrayString[0]) \(nameArrayString[3])")
+
+let mySpace = " "
+print("Answer = \(nameArrayString[0] + mySpace + nameArrayString[3])")
+
+// การหาขนาดของ array
+let length = nameArrayString.count
+print ("ขนาดของ Array = \(length)")
+
+// การเพิ่มสมาชิก Array
+nameArrayString.append("Doramon")
+
+//  การลบ สมาชิก Array
+nameArrayString
+nameArrayString.remove(at: 1)
+nameArrayString
