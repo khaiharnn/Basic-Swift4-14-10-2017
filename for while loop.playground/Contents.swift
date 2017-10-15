9d09260e8f716ba49297ff23d945b40c3d41e915
khaiharnn <khaiharnn@gmail.com>
2017-10-15 11:55:54 +0700

    While loop

--- for while loop.playground/Contents.swift
+++ for while loop.playground/Contents.swift
@@ -6,11 +6,25 @@ import UIKit
 
 let nameString  =  ["Doraemon","Nobita","Tsuneo","Master Ung"]
 
+
 var timesInt = 0
 
-for atrFriend in nameString {
-    
-    print ("Name of Friend == \(atrFriend)")
-    timesInt += 1
-    print("จำนวณรอบที่ \(timesInt)")
-}
+//for atrFriend in nameString {
+//
+//    print ("Name of Friend == \(atrFriend)")
+//    timesInt += 1
+//    print("จำนวณรอบที่ \(timesInt)")
+//}
+
+//let numberInt = [1,2,3,4,5]
+//for PositionArray in numberInt {
+//    print("Array[\(PositionArray)] == \(numberInt[PositionArray-1])")
+//}
+
+// While loop
+//var factorInt = 1 // last factorInt = 12
+//while (factorInt <= 12) {
+//    print("รอบที่ \(factorInt)")
+//    factorInt += 1
+//}
+
