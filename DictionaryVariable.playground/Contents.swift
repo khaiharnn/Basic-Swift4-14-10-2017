105dd4396cc8f190b677eade376f8c600ed96768
khaiharnn <khaiharnn@gmail.com>
2017-10-15 11:31:11 +0700

    for loop type String

--- DictionaryVariable.playground/Contents.swift
+++ DictionaryVariable.playground/Contents.swift
@@ -18,3 +18,79 @@ print("จำนวนสมาชิก => \(myDictionaryString.count)")
 //การลบ
 myDictionaryString.removeValue(forKey: "key2")
 print (myDictionaryString)
+
+
+
+
+
+/////////// ที่คิดเอง
+////
+////  ViewController.swift
+////  LoginDictionary
+////
+////  Created by Patinya Sathaporncharoenying on 10/14/2560 BE.
+////  Copyright © 2560 khaiharnn. All rights reserved.
+////
+//
+//import UIKit
+//
+//class ViewController: UIViewController {
+//
+//    var nameString = ("First")
+//    var pwdString = ("1234")
+//    var result = ""
+//
+//
+//    @IBOutlet weak var showLabel1: UILabel!
+//
+//    @IBOutlet weak var showLabel2: UILabel!
+//
+//
+//    @IBOutlet weak var showLabel3: UILabel!
+//
+//    @IBOutlet weak var addTextfield1: UITextField!
+//
+//
+//    @IBOutlet weak var addTextfield2: UITextField!
+//
+//
+//
+//
+//    @IBAction func clickButton(_ sender: Any) {
+//
+//        print("Click Success")
+//
+//        nameString = addTextfield1.text!
+//        pwdString = addTextfield2.text!
+//
+//        if nameString == "First" && pwdString == "1234" {
+//
+//            result = "Success"
+//            showLabel3.text = result
+//
+//            print ("Success")
+//        }
+//        else {
+//            result = "Fail"
+//            showLabel3.text = result
+//            print ("Fail")
+//        }
+//
+//
+//
+//
+//        func viewDidLoad() {
+//            super.viewDidLoad()
+//            // Do any additional setup after loading the view, typically from a nib.
+//        }
+//
+//        func didReceiveMemoryWarning() {
+//            super.didReceiveMemoryWarning()
+//            // Dispose of any resources that can be recreated.
+//        }
+//
+//
+//    }
+//
+//}
+
---
+++ for while loop.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//sample how to use for loop
+
+let nameString  =  ["Doraemon","Nobita","Tsuneo","Master Ung"]
+
+var timesInt = 0
+
+for atrFriend in nameString {
+    
+    print ("Name of Friend == \(atrFriend)")
+    timesInt += 1
+    print("จำนวณรอบที่ \(timesInt)")
+}
